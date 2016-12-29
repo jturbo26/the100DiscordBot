d112ae0e3615d79b00f443961a20741af455ac3b
Joseph Turbin <jturbin@its.jnj.com>
2016-12-29 11:37:50 -0500

    Changed something fn to be more readable
    modified individual stats commands

--- commands/stats.js
+++ commands/stats.js
@@ -35,65 +35,51 @@ const stats = msg => {
 		const bodyContent = JSON.parse(body);
 		const quickplayData = bodyContent.us.stats.quickplay;
 		const competitiveData = bodyContent.us.stats.competitive;
+		const emptyObjectChecker = Object.getOwnPropertyNames(quickplayData);
+		if (emptyObjectChecker.length === 0) {
+			msg.reply("Sorry. This user doesn't have any data.");
+			return;
+		}
 		msg.reply("```Markdown" +
 			"\n#Here are basic stats for " + msgStatsContent +
-			"\nLevel: "  + quickplayData.overall_stats.prestige + quickplayData.overall_stats.level + "```" +
-			"\nCompRank: " + (competitiveData.overall_stats.comprank !== null ? competitiveData.overall_stats.comprank : "Not Ranked") + ")");
+			"\nLevel: "  + quickplayData.overall_stats.prestige + quickplayData.overall_stats.level +
+			"\nCompRank: " + (competitiveData.overall_stats.comprank !== null ? competitiveData.overall_stats.comprank : "Not Ranked") + "```");
 	}
 
 	const getCompUserStats = (error, response, body) => {
 		const bodyContent = JSON.parse(body);
 		const competitiveData = bodyContent.us.stats.competitive;
-		const emptyObjectChecker = Object.getOwnPropertyNames(competitiveData);
-		if (response.statusCode === 200) {
-			if (emptyObjectChecker.length === 0) {
-				msg.reply("Sorry. This user doesn't have any competitive data.");
-				return;
-			}
-			msg.reply("```Markdown" +
-				"\n#Here are competitive stats for " + msgCompStatsContent +
-				" (CompRank: " + (competitiveData.overall_stats.comprank !== null ? competitiveData.overall_stats.comprank : "Not Ranked") + ")" +
-				"\nAverage Deaths: " + competitiveData.average_stats.deaths_avg + "```");
-		}
-		else {
-			msg.reply("Error! Status Code = " + response.statusCode);
-		}
+		msg.reply("```Markdown" +
+			"\n#Here are competitive stats for " + msgCompStatsContent +
+			" (CompRank: " + (competitiveData.overall_stats.comprank !== null ? competitiveData.overall_stats.comprank : "Not Ranked") + ")" +
+			"\nAverage Deaths: " + competitiveData.average_stats.deaths_avg + "```");
 	}
 
 	const getAvgUserStats = (error, response, body) => {
 		const bodyContent = JSON.parse(body);
 		const competitiveData = bodyContent.us.stats.competitive;
 		msg.reply("```Markdown" +
-			"\n#Here are competitive stats for " + msgAvgStatsContent + " (CompRank: " + competitiveData.overall_stats.comprank + ")" +
-			'\n![Avatar]("' + competitiveData.overall_stats.avatar + ')' +
-			"\nAverage Deaths: " + competitiveData.average_stats.deaths_avg + "```");
+			"\n#This command is not ready yet. Still in development.");
 	}
 
-	// // Here are the actual calls to the API using npm request.
-	// if (msg.content.startsWith('$' + 'stats')) {
-	// 	request(urls.statsOptions, getUserStats);
-	// }
-	// else if (msg.content.startsWith('$' + 'compstats')) {
-	// 	request(urls.compStatsOptions, getCompUserStats);
-	// }
-	// else if (msg.content.startsWith('$' + 'avgstats')) {
-	// 	request(urls.avgStatsOptions, getUserStats);
-	// }
-
-	someThing = fn => (error, response, body) => {
+	//Check the JSON to ensure the user exists. If the first
+	//character of the body is '{' then the user doesn't exist.
+	//Then use the function that was passed in below to display
+	//the users data in discord.
+	lookupCorrectFn = fn => (error, response, body) => {
 		if (body[0] === '{') {
 			fn(error, response, body)
 		}
 	}
 
 	if (msg.content.startsWith('$' + 'stats')) {
-		request(urls.statsOptions, someThing(getUserStats));
+		request(urls.statsOptions, lookupCorrectFn(getUserStats));
 	}
 	else if (msg.content.startsWith('$' + 'compstats')) {
-		request(urls.compStatsOptions, someThing(getCompUserStats));
+		request(urls.compStatsOptions, lookupCorrectFn(getCompUserStats));
 	}
 	else if (msg.content.startsWith('$' + 'avgstats')) {
-		request(urls.avgStatsOptions, someThing(getUserStats));
+		request(urls.avgStatsOptions, lookupCorrectFn(getAvgUserStats));
 	}
 
 }
