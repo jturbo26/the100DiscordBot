a8415659a25036c41899824b456153316db89640
Unknown <bwillms@rogers.com>
2017-01-04 20:42:56 -0500

    Quick Play stats added to stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const moment = require('moment');
 
 const apiV3UrlRoot = 'https://owapi.net/api/v3';
 
@@ -41,9 +42,45 @@ const stats = msg => {
 			return;
 		}
 		msg.reply("```Markdown" +
-			"\n#Here are basic stats for " + msgStatsContent +
-			"\nLevel: "  + (quickplayData.overall_stats.prestige !== 0 ? quickplayData.overall_stats.prestige : '') + quickplayData.overall_stats.level +
-			"\nComp Rank: " + (competitiveData.overall_stats.comprank !== null ? competitiveData.overall_stats.comprank : "Not Ranked") + "```");
+			"\n#Here are quick play stats for " + msgStatsContent +
+			" (Level: "  + (quickplayData.overall_stats.prestige !== 0 ? quickplayData.overall_stats.prestige : '') + quickplayData.overall_stats.level +
+			" | Games Won: " + quickplayData.overall_stats.wins +
+			" | QP Time Played: " + quickplayData.game_stats.time_played + " hours)" +
+			"\n\n#Lifetime Totals" +
+			"\nMedals: " + quickplayData.game_stats.medals.toLocaleString() + " (G:" + quickplayData.game_stats.medals_gold.toLocaleString() + " S:" + quickplayData.game_stats.medals_silver.toLocaleString() + " B:" + quickplayData.game_stats.medals_bronze.toLocaleString() + ")" +
+			"\nVoting Cards: " + quickplayData.game_stats.cards.toLocaleString() +
+			"\n\nDamage Done: " + quickplayData.game_stats.damage_done.toLocaleString() +
+			"\nHealing Done: " + quickplayData.game_stats.healing_done.toLocaleString() +
+			"\n\nEliminations: " + quickplayData.game_stats.eliminations.toLocaleString() +
+			"\nDeaths: " + quickplayData.game_stats.deaths.toLocaleString() +
+			"\nEliminations per Death: " + quickplayData.game_stats.kpd.toLocaleString() +
+			"\n\nObjective Kills: " + quickplayData.game_stats.objective_kills.toLocaleString() +
+			"\nEnvironmental Kills: " + quickplayData.game_stats.environmental_kills.toLocaleString() +
+			"\nEnvironmental Deaths: " + quickplayData.game_stats.environmental_deaths.toLocaleString() +
+			"\nFinal Blows: " + quickplayData.game_stats.final_blows.toLocaleString() +
+			"\nMelee Final Blows: " + quickplayData.game_stats.melee_final_blows.toLocaleString() +
+			"\nSolo Kills: " + quickplayData.game_stats.solo_kills.toLocaleString() +
+			"\nMultikills: " + quickplayData.game_stats.multikills.toLocaleString() +
+			"\n\nOffensive Assists: " + quickplayData.game_stats.offensive_assists.toLocaleString() +
+			"\nDefensive Assists: " + quickplayData.game_stats.defensive_assists.toLocaleString() +
+			"\n\nTime Spent on Fire: " + moment().startOf('day').seconds(quickplayData.game_stats.time_spent_on_fire * 3600).format('H:mm:ss') + " (" + (Math.round((quickplayData.game_stats.time_spent_on_fire / quickplayData.game_stats.time_played) * 1000) / 10)  + "% of the time)" +
+			"\nObjective Time: " + moment().startOf('day').seconds(quickplayData.game_stats.objective_time * 3600).format('H:mm:ss') +
+			"\nTeleporter / Shield Generators Destroyed: " + quickplayData.game_stats.teleporter_pads_destroyed.toLocaleString() +
+
+			"\n\n#Lifetime Records" +
+			"\nDamage Done: " + quickplayData.game_stats.damage_done_most_in_game.toLocaleString() +
+			"\nHealing Done: " + quickplayData.game_stats.healing_done_most_in_game.toLocaleString() +
+			"\n\nEliminations: " + quickplayData.game_stats.eliminations_most_in_game.toLocaleString() +
+			"\n\nObjective Kills: " + quickplayData.game_stats.objective_kills_most_in_game.toLocaleString() +
+			"\nFinal Blows: " + quickplayData.game_stats.final_blows_most_in_game.toLocaleString() +
+			"\nMelee Final Blows: " + quickplayData.game_stats.melee_final_blows_most_in_game.toLocaleString() +
+			"\nSolo Kills: " + quickplayData.game_stats.solo_kills_most_in_game.toLocaleString() +
+			"\nMultikill: " + quickplayData.game_stats.multikill_best.toLocaleString() +
+			"\n\nOffensive Assists: " + quickplayData.game_stats.offensive_assists_most_in_game.toLocaleString() +
+			"\nDefensive Assists: " + quickplayData.game_stats.defensive_assists_most_in_game.toLocaleString() +
+			"\n\nTime Spent on Fire: " + moment().startOf('day').seconds(quickplayData.game_stats.time_spent_on_fire_most_in_game * 3600).format('H:mm:ss') +
+			"\nObjective Time: " + moment().startOf('day').seconds(quickplayData.game_stats.objective_time_most_in_game * 3600).format('H:mm:ss') +
+		  "```");
 	}
 
 	const getCompUserStats = (error, response, parsedBody) => {
@@ -81,9 +118,12 @@ const stats = msg => {
 	//the users data in discord.
 	lookupCorrectFn = fn => (error, response, body) => {
 		const parsedBody = JSON.parse(body);
-		if (parsedBody.error !== 404) {
+		if (parsedBody.error !== 404 || parsedBody.error !== 500) {
 			fn(error, response, parsedBody)
 		}
+		else if (parsedBody.error === 500 ) {
+					msg.reply("Bwa Bwa Bwa Bwa. There is an issue with the Overwatch API. Try again later.");
+				}
 		else {
 			msg.reply("Bweeeeeeeeeeeoh. There was an error finding that user. Usernames are case sensitive. Try again.");
 		}
