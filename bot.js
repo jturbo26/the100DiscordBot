9393af085d24a6e816d8d1490fc198344b8e6c21
Cherish Khang <cherishkhang@gmail.com>
2017-11-22 18:19:36 -0500

    Update new member bot DM message and update channels for new member messages

--- bot.js
+++ bot.js
@@ -29,43 +29,34 @@ bot.on('guildMemberAdd', (guildMember) =>
         embed:
         {
             color: 65380,
-            description: `
-        ${guildMember.user} Welcome to ***Charlie Company 337***. We are a super casual
-        gaming group that has a ton of fun together. We're very active here
-        in Discord, have games going every night and group events throughout the month.
+            description: `Welcome to **Charlie Company 337**! We are a super casual gaming group that has a ton of fun together. We're very active here in Discord, have games going every night and group events throughout the month.
 
-        __There are a few things you should do to be successful in our group:__
+__**There are a few things you should do to be successful in our group:**__
 
-        1. If you haven't already, join our group on the100. This is where we schedule
-	        our games. You can still do PUGs in Discord, but this is the core of our group.
-	        https://www.the100.io/g/3140
+     1. If you haven't already, join our group on the100. This is where we schedule our games. You can still do PUGs in Discord, but this is the core of our group. https://www.the100.io/g/3140
 
-        2. Be sure to right-click your name in Discord and select "Change Nickname". If
-	        your main game is a blizzard game use your BattlenetId. If it's a Steam game like
-	        PUBG please change your nickname to match your Steam name and format like this: "Username (Steam)"
+     2. Be sure to right-click your name in Discord and select "Change Nickname". If your main game is a blizzard game use your BattlenetId. If it's a Steam game like PUBG please change your nickname to match your Steam name and format like this: "Username (Steam)"
 
-        3. Head over to ${bot.channels.find('name', 'introductions')} and take a moment to tell us about yourself. What
-	        games do you play? What else do you like to do? Feel free to just say hi. We're a welcoming group.
+     3. Familiarize yourself with our ${bot.channels.find('name', 'rules_and_info')}.
 
-        4. Check out ${bot.channels.find('name', 'use_enslaved_omnics_here')} and use the $games command to see what's on the schedule.
+     4. Head over to ${bot.channels.find('name', 'introductions')} and take a moment to tell us about yourself. What games do you play? What else do you like to do? Feel free to just say hi. We're a welcoming group.
 
-        5. Read ${bot.channels.find('name', 'rules-missionstatement')} to learn more.
+     5. Check out ${bot.channels.find('name', 'use_enslaved_omnics_here')} and use the $games command to see what's on the schedule.
 
-        That's it. If you have any questions, please let a Core Member or Moderator know.
-		        `
+That's it. If you have any questions, please let a member of the leadership team know.`
         }
     });
 
-    const generalChannel = guildMember.guild.channels.find('name', 'l-----------general-----------l');
+    const newMemberChannel = guildMember.guild.channels.find('name', 'welcome_new_members');
 
-    const coreChannel = guildMember.guild.channels.find('name', 'company_leadership');
+    const leadershipChannel = guildMember.guild.channels.find('name', 'company_leadership');
 
     const memberLogChannel = guildMember.guild.channels.find('name', 'member_log');
 
-    // Post a message in general/company_leadership/member_log notifying users of new member.
-    generalChannel.send(`Hey everyone! We have a new member. Please welcome ${guildMember.user} to our group! ${guildMember.user} please check your Direct Messages for an important message from the CC337 moderators.`);
+    // Post a message in welcome_new_members/company_leadership/member_log notifying users of new member.
+    newMemberChannel.send(`Hey everyone! We have a new member. Please welcome ${guildMember.user} to our group! ${guildMember.user}, please read the post at the top of this channel for more information on how to get promoted to Grunt and be given access to the rest of the Discord. Happy gaming!`);
 
-    coreChannel.send(`Hey Core Members! We have a new member. Please be sure to welcome them and encourage them to participate! New Member = ${guildMember.user}`);
+    leadershipChannel.send(`Hey leadership team! We have a new member. Please be sure to welcome them and encourage them to participate! New Member = ${guildMember.user}`);
 
     memberLogChannel.send(`New Member = ${guildMember.user}`);
 });
